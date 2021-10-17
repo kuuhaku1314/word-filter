4f7f395c173276d26c8011d300e487342de2b6cf
kuuhaku1314 <q215836858@gmail.com>
2021-10-17 21:36:33 +0800

    修改名称

--- tree.go
+++ tree.go
@@ -1,8 +1,8 @@
 package tree
 
 type Node struct {
-	Value string
-	Child map[string]*Node
+	Value    string
+	Children map[string]*Node
 }
 
 type Tree struct {
@@ -11,19 +11,19 @@ type Tree struct {
 
 // BuildTree 通过给定敏感词数组建树
 func BuildTree(words []string) (*Tree, error) {
-	root := &Node{Child: map[string]*Node{}}
+	root := &Node{Children: map[string]*Node{}}
 	for _, word := range words {
 		runes := []rune(word)
 		node := root
 		for _, r := range runes {
-			if node.Child == nil {
-				node.Child = map[string]*Node{}
+			if node.Children == nil {
+				node.Children = map[string]*Node{}
 			}
-			if v, ok := node.Child[string(r)]; ok{
+			if v, ok := node.Children[string(r)]; ok {
 				node = v
 			} else {
 				temp := &Node{Value: string(r)}
-				node.Child[string(r)] = temp
+				node.Children[string(r)] = temp
 				node = temp
 			}
 		}
@@ -31,7 +31,7 @@ func BuildTree(words []string) (*Tree, error) {
 	return &Tree{Root: root}, nil
 }
 
-func (t *Tree)FindFirstMatchedWord(text string) (ok bool, word string) {
+func (t *Tree) FindFirstMatchedWord(text string) (ok bool, word string) {
 	runes := []rune(text)
 	for i := 0; i < len(runes); i++ {
 		if ok, word := t.findFirstMatchedWord(runes[i:]); ok {
@@ -41,15 +41,15 @@ func (t *Tree)FindFirstMatchedWord(text string) (ok bool, word string) {
 	return false, ""
 }
 
-func (t *Tree)findFirstMatchedWord(runes []rune) (ok bool, word string) {
+func (t *Tree) findFirstMatchedWord(runes []rune) (ok bool, word string) {
 	cur := t.Root
 	end := 0
 	for i, r := range runes {
-		if cur.Child == nil {
+		if cur.Children == nil {
 			end = i
 			break
 		}
-		if next := cur.Child[string(r)]; next != nil {
+		if next := cur.Children[string(r)]; next != nil {
 			cur = next
 		} else {
 			return false, ""
